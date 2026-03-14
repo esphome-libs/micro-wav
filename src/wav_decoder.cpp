7de08261ede26f9f1950ec6a95329cbd163210b6
Kevin Ahrendt <kahrendt@gmail.com>
2026-03-14 10:46:41 -0400

    add basic doxygen comment at top of file

--- src/wav_decoder.cpp
+++ src/wav_decoder.cpp
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/// @file wav_decoder.cpp
+/// @brief WAVDecoder implementation
+
 #include "micro_wav/wav_decoder.h"
 
 #include <algorithm>
