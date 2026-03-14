13cf209de4e2322572ac5b9d1a2c415e9157b059
Kevin Ahrendt <kahrendt@gmail.com>
2026-03-14 10:53:54 -0400

    revert the cpp file doxygen comment

--- src/wav_decoder.cpp
+++ src/wav_decoder.cpp
@@ -12,9 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/// @file wav_decoder.cpp
-/// @brief WAVDecoder implementation
-
 #include "micro_wav/wav_decoder.h"
 
 #include <algorithm>
